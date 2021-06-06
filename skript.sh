1aa0914f6128981aef060292740d75366f264fc5
Olga Lukashova <Modema2006@yandex.ru>
2021-06-06 10:07:44 +0300

    add skript

---
+++ skript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir Week_nedelya
+cd Week_nedelya
+mkdir Weekdays Monday Weekend
+cd Weekdays
+touch Monday.txt Wednesday.tst Thursday.json Friday.json
+mkdir Morning Day Evening
+ls -la
+mv Wednesday.txt Friday.json ///Monday
